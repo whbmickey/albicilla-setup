a6177554d63d138e9a89fda869d83998c37dfab9
Sirius <fhc023@gmail.com>
2016-03-31 10:34:36 +0800

    upgrade to node:5.7

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGES="jwilder/nginx-proxy node:0.10 tradesparq/php-apache2 digitallyseamless/nodejs-bower-grunt:0.10"
+IMAGES="jwilder/nginx-proxy node:5.7 tradesparq/php-apache2 digitallyseamless/nodejs-bower-grunt:0.10"
 for image in $IMAGES; do
   docker pull $image
 done
